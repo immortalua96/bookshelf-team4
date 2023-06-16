d88fbcee93c5ef22ef09e5a44be305ac67192d2b
immortalua96 <romanshvagla@icloud.com>
2023-06-16 14:51:03 +0200

    fetch два запита на всі категорії

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/sideBar/renderListCategories';
 import './js/sideBar/renderListSupportUkraine';
+import './js/sideBar/main/renderBestBooks';
--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 const CATEGORIES_URL =
   'https://books-backend.p.goit.global/books/category-list ';
+const BEST_URL = 'https://books-backend.p.goit.global/books/top-books ';
 export async function fetchCategories() {
   try {
     const response = await axios.get(`${CATEGORIES_URL}`);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+export async function fetchBestBooks() {
+  try {
+    const response = await axios.get(`${BEST_URL}`);
     const data = response.data;
     return data;
   } catch (error) {
