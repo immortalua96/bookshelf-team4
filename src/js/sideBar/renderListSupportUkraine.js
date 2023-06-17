9c07897eb53a46b5549abbd76c7a15d567091f9f
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-06-17 10:55:41 +0100

    sideBar_scss

--- src/js/sideBar/renderListSupportUkraine.js
+++ src/js/sideBar/renderListSupportUkraine.js
@@ -1,15 +1,12 @@
 import { refs } from '../refs';
 import supportUkraine from '../supportUkraine.json';
 function renderListSupportUkraine(data) {
-  refs.listSuportUkraine.style.listStyle="none"
 
-  refs.listSuportUkraine.style.backgroundColor="#4F2EE8"
-  refs.listSuportUkraine.style.color="#FFFFFF"
 let i = 0;
    const markup = data
   .map(
     ({ title, url, img }) => 
-            `<li><p>${addLeadingZero(i+=1)}</p><a href=${url} target="_blank" rel="noopener noreferrer"><img src="${img}" alt="${title}"></a></li>`
+            `<li class="listItemSupport"><p class="counterPoint">${addLeadingZero(i+=1)}</p><a href=${url} target="_blank" rel="noopener noreferrer"><img src="${img}" alt="${title}"></a></li>`
     )
           .join('');
   refs.listSuportUkraine.insertAdjacentHTML('beforeend', markup);
@@ -20,6 +17,3 @@ function addLeadingZero(value) {
   return String(value).padStart(2, 0)
 }
 
-// const images = JSON.parse(supportUkraine);
-// console.log(images);
-// console.log(users[1].name); // => Ervin Howell
