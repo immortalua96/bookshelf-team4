033169842331e198b5c95490d717eceda67cdab6
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-06-17 01:05:09 +0100

    editing list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/sideBar/renderListCategories';
 import './js/sideBar/renderListSupportUkraine';
 import './js/sideBar/main/renderBestBooks';
+
--- src/js/sideBar/renderListSupportUkraine.js
+++ src/js/sideBar/renderListSupportUkraine.js
@@ -1,13 +1,25 @@
 import { refs } from '../refs';
 import supportUkraine from '../supportUkraine.json';
-refs.listSuportUkraine;
 function renderListSupportUkraine(data) {
-  const markup = data
-    .map(
-      ({ title, url }) =>
-        `<li><a href=${url} target="_blank" rel="noopener noreferrer"><img src="../../images/1.png" alt="${title}"></a></li>`
+  refs.listSuportUkraine.style.listStyle="none"
+
+  refs.listSuportUkraine.style.backgroundColor="#4F2EE8"
+  refs.listSuportUkraine.style.color="#FFFFFF"
+let i = 0;
+   const markup = data
+  .map(
+    ({ title, url, img }) => 
+            `<li><p>${addLeadingZero(i+=1)}</p><a href=${url} target="_blank" rel="noopener noreferrer"><img src="${img}" alt="${title}"></a></li>`
     )
-    .join('');
+          .join('');
   refs.listSuportUkraine.insertAdjacentHTML('beforeend', markup);
 }
 renderListSupportUkraine(supportUkraine);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0)
+}
+
+// const images = JSON.parse(supportUkraine);
+// console.log(images);
+// console.log(users[1].name); // => Ervin Howell
