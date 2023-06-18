60c3fb3c5c788566071374a30d6db5146a520035
immortalua96 <romanshvagla@icloud.com>
2023-06-18 17:30:24 +0200

    ap main

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,9 +3,8 @@ export const refs = {
   listSuportUkraine: document.getElementById('jsListSuportUkraine'),
   inputSwitch: document.getElementById('switchMode'),
   mainPage: document.getElementById('mainPage'),
-  // ScrUpButton: document.getElementById('scrollUpBtn'),
   itemSupportUkraine: document.querySelector('listItemSupport'),
-  scrDownBtn: document.getElementById('go-down')
+  scrDownBtn: document.getElementById('go-down'),
   ScrUpButton: document.getElementById('scrollUpBtn'),
   popup: document.querySelector('.popup'),
 };
