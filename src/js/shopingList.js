a2c0df18c61e83945f3b07d7e7d5753d314e8e14
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-06-18 15:57:53 +0200

    list

--- src/js/shopingList.js
+++ src/js/shopingList.js
@@ -1,3 +1,5 @@
+import defaultImage from '../images/10.png';
+
 const books = [
   //   {
   //     img: '',
@@ -220,7 +222,7 @@ function renderEmpty() {
       <p class="emptyText">
         This page is empty, add some books and proceed to order.
       </p>
-      <img class="emptyImg" src="/10.48ab74dc.png" alt="" />
+      <img class="emptyImg" src="${defaultImage}" alt="" />
     </div>`;
 
   shoppingList.innerHTML = markup;
