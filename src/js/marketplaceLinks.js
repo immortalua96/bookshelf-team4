d58c6d8c2f371e482dec0770d08d459faa80addf
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-06-22 14:20:36 +0200

    .

--- src/js/marketplaceLinks.js
+++ src/js/marketplaceLinks.js
@@ -4,7 +4,7 @@ import iconBookShop from '/src/images/bookshop.png';
 export function renderMarketPlaceLink(data) {
   return `<ul class="listMarkeplace">
     <li class="itemMaretplace"><a class="linkMarketPlace" href="${data[0].url}" target="_blank" rel="noopener noreferrer">
-      <img class="imgMarketPlace" src="${iconAmazon}" alt="${data[0].name}">
+      <img class="imgMarketPlace imgMarketPlace_amazon" src="${iconAmazon}" alt="${data[0].name}">
     </a></li>
     <li class="itemMaretplace"><a class="linkMarketPlace" href="${data[1].url}" target="_blank" rel="noopener noreferrer">
       <img class="imgMarketPlace" src="${iconAppleBooks}" alt="${data[1].name}">
